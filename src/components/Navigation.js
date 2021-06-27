9708f71f795c0f7e1380d0ebbd352e3e291bbc8b
joonghyun <joongh@ewhain.net>
2021-06-27 18:31:10 +0900

    2.6 Log Out

---
+++ src/components/Navigation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navigation = () => (
+   <nav>
+      <ul>
+         <li>
+            <Link to="/">Home</Link>
+         </li>
+         <li>
+            <Link to="/Profile">My Profile</Link>
+         </li>
+      </ul>
+   </nav>
+);
+export default Navigation;
