b025dee8575adcde98e00434a93fef14d0636ef5
joonghyun <joongh@ewhain.net>
2021-06-27 17:35:28 +0900

    2.4 Log In

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -5,6 +5,7 @@ const Auth = () => {
    const [email, setEmail] = useState('');
    const [password, setPassword] = useState('');
    const [newAccount, setNewAccount] = useState(true);
+   const [error, setError] = useState('');
    const onChange = (event) => {
       const {
          target: { name, value },
@@ -32,9 +33,11 @@ const Auth = () => {
          }
          console.log(data);
       } catch (error) {
-         console.log(error);
+         setError(error.message);
       }
    };
+   const toggleAccount = () => setNewAccount((prev) => !prev);
+
    return (
       <div>
          <form onSubmit={onSubmit}>
@@ -56,9 +59,13 @@ const Auth = () => {
             />
             <input
                type="submit"
-               value={newAccount ? 'Create Account' : 'Log In'}
+               value={newAccount ? 'Create Account' : 'Sign In'}
             />
+            {error}
          </form>
+         <span onClick={toggleAccount}>
+            {newAccount ? 'Sign in' : 'Create Account'}
+         </span>
          <div>
             <button>Continue with Google</button>
             <button>Continue with Github</button>
