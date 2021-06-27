c7b8ad5b9428b96d064cc2b9870046355a2f36d0
joonghyun <joongh@ewhain.net>
2021-06-27 15:51:07 +0900

    2.0 Using Firebase Auth

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import AppRouter from "./Router";
+import React, { useState } from 'react';
+import AppRouter from "components/Router";
+import fBase from "fBase";
+import { authService } from "fBase";
 
 function App() {
-  return <AppRouter />;
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; Nwitter {new Date().getFullYear()}</footer>
+    </>
+  );
 }
 
 export default App;
