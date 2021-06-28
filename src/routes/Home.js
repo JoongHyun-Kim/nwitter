36838738eb228660b003f47ac0dc5a0664286838
joonghyun <joongh@ewhain.net>
2021-06-28 13:20:03 +0900

    3.1 Nweeting

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,35 @@
-import React from "react";
+import { dbService } from 'fBase';
+import React, { useState } from 'react';
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+   const [nweet, setNweet] = useState('');
+   const onSubmit = async (event) => {
+      event.preventDefault();
+      await dbService.collection('nweets').add({
+         nweet,
+         createdAt: Date.now(),
+      });
+      setNweet('');
+   };
+   const onChange = (event) => {
+      const {
+         target: { value },
+      } = event;
+      setNweet(value);
+   };
+   return (
+      <div>
+         <form onSubmit={onSubmit}>
+            <input
+               value={nweet}
+               onChange={onChange}
+               type="text"
+               placeholder="What's on your mind?"
+               maxLength={120}
+            />
+            <input type="submit" value="Nweet" />
+         </form>
+      </div>
+   );
+};
 export default Home;
