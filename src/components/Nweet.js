9dcd59a455c34c439309b1d4c45c9dce672693e4
joonghyun <joongh@ewhain.net>
2021-06-28 18:06:29 +0900

    4.3 File URL and Nweet

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -47,6 +47,13 @@ const Nweet = ({ nweetObj, isOwner }) => {
          ) : (
             <>
                <h4>{nweetObj.text}</h4>
+               {nweetObj.attachmentUrl && (
+                  <img
+                     src={nweetObj.attachmentUrl}
+                     width="50px"
+                     height="50px"
+                  />
+               )}
                {isOwner && (
                   <>
                      <button onClick={onDeleteClick}>Delete Nweet</button>
