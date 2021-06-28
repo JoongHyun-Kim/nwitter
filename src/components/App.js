a660e88f5dd5be1b42e12fb73c5571a8ec30edbc
joonghyun <joongh@ewhain.net>
2021-06-28 15:57:51 +0900

    3.6 Recap

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,11 @@ import { authService } from 'fBase';
 
 function App() {
    const [init, setInit] = useState(false);
-   const [isLoggedIn, setIsLoggedIn] = useState(false);
    const [userObj, setUserObj] = useState(null);
    useEffect(() => {
       authService.onAuthStateChanged((user) => {
          if (user) {
-            setIsLoggedIn(true);
             setUserObj(user);
-         } else {
-            setIsLoggedIn(false);
          }
          setInit(true);
       });
@@ -21,7 +17,7 @@ function App() {
    return (
       <>
          {init ? (
-            <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} />
+            <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} />
          ) : (
             'Initializing...'
          )}
