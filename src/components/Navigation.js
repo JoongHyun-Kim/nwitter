1a006c5dda083a0d8b55eec7bd3c386344382263
joonghyun <joongh@ewhain.net>
2021-06-29 13:42:51 +0900

    5.1 Update Profile

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Navigation = () => (
+const Navigation = ({ userObj }) => (
    <nav>
       <ul>
          <li>
             <Link to="/">Home</Link>
          </li>
          <li>
-            <Link to="/Profile">My Profile</Link>
+            <Link to="/Profile">{userObj.displayName}의 Profile</Link>
          </li>
       </ul>
    </nav>
