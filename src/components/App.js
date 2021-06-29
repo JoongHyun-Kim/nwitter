e96724f402144521ed0277fbcd0428387f3f995f
joonghyun <joongh@ewhain.net>
2021-06-29 14:04:42 +0900

    5.2 Update Profile Bugfix

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import AppRouter from 'components/Router';
-import fBase from 'fBase';
 import { authService } from 'fBase';
 
 function App() {
@@ -9,15 +8,33 @@ function App() {
    useEffect(() => {
       authService.onAuthStateChanged((user) => {
          if (user) {
-            setUserObj(user);
+            setUserObj({
+               displayName: user.displayName,
+               uid: user.uid,
+               updateProfile: (args) => user.updateProfile(args),
+            });
+         } else {
+            setUserObj(null);
          }
          setInit(true);
       });
    }, []);
+   const refreshUser = () => {
+      const user = authService.currentUser;
+      setUserObj({
+         displayName: user.displayName,
+         uid: user.uid,
+         updateProfile: (args) => user.updateProfile(args),
+      });
+   };
    return (
       <>
          {init ? (
-            <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} />
+            <AppRouter
+               refreshUser={refreshUser}
+               isLoggedIn={Boolean(userObj)}
+               userObj={userObj}
+            />
          ) : (
             'Initializing...'
          )}
